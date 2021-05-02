3bd44d62165efdde1cafc5f5f12861860fb0f1a5
Яна Белых <61792507+yanabelykh18@users.noreply.github.com>
2021-05-02 21:19:54 +0500

    Update App.js

--- quadratic equation/App.js
+++ quadratic equation/App.js
@@ -1,27 +1,28 @@
-alert("Квадратное уравнение ax^2+bx+c");
-var a = prompt ("Введите коэффициент a", " ");
-var b = prompt ("Введите коэффициент b", " ");
-var c = prompt ("Введите коэффициент c", " ");
-D = (b*b)-(4*a*c);
+alert("Квадратное уравнение вида ax^2+bx+c");
+var a = prompt ("Введите значения a", " ");
+var b = prompt ("Введите значения b", " ");
+var c = prompt ("Введите значения c", " ");
+Dis = (b*b)-(4*a*c);
 if (c != 0 && a != 0 && b != 0)
 {	
-if (D > 0)
+if (Dis > 0)
 		{
-			res1 = (-b + Math.sqrt(D))/2*a;
-			res2 = (-b - Math.sqrt(D))/2*a;
+			res1 = (-b + Math.sqrt(Dis))/2*a;
+			res2 = (-b - Math.sqrt(Dis))/2*a;
 			document.write("x1 = ",res1 );
-			document.write("x2 = ",res2 );;
+			document.write(" x2 = ",res2 );;
 		}
-	else if (D == 0)
+	else if (Dis == 0)
 		{
 			res1 = (-b/2*a);
 			document.write("x1 = ",res1 );
+		
 		}
-	else if (D < 0)
+	else if (Dis < 0)
 		{
-			D = Math.abs(D);
-			res1 = miss*b + "/" + 2*a + "+" + D**(1/2) + "*i" + "/" + 2*a;
-			res2 = miss*b + "/" + 2*a + "-" + D**(1/2) + "*i" + "/" + 2*a;
+			Dis = Math.abs(Dis);
+			res1 = miss*b + "/" + 2*a + "+" + Dis**(1/2) + "*i" + "/" + 2*a;
+			res2 = miss*b + "/" + 2*a + "-" + Dis**(1/2) + "*i" + "/" + 2*a;
 			document.write("x1 = ",res1 );
 			document.write(" x2 = ",res2 );
 		}
@@ -29,31 +30,35 @@ if (D > 0)
 
  if (c == 0)
 {
-	if (D > 0)
+	if (Dis > 0)
 		{
-			res1 = (-b + Math.sqrt(D))/2*a;
-			res2 = (-b - Math.sqrt(D))/2*a;
+			res1 = (-b + Math.sqrt(Dis))/2*a;
+			res2 = (-b - Math.sqrt(Dis))/2*a;
 			document.write("x1 = ",res1 );
 			document.write(" x2 = ",res2 );
+	
 		}
-	else if (D == 0)
+	else if (Dis == 0)
 		{
 			res1 = (-b/2*a);
 			document.write("x1 = ",res1 );
+			
 		}
-	else if (D < 0)
+	else if (Dis < 0)
 		{
-			D = Math.abs(D);
-			res1 = miss*b + "/" + 2*a + "+" + D**(1/2) + "*i" + "/" + 2*a;
-			res2 = miss*b + "/" + 2*a + "-" + D**(1/2) + "*i" + "/" + 2*a;
+			Dis = Math.abs(Dis);
+			res1 = miss*b + "/" + 2*a + "+" + Dis**(1/2) + "*i" + "/" + 2*a;
+			res2 = miss*b + "/" + 2*a + "-" + Dis**(1/2) + "*i" + "/" + 2*a;
 			document.write("x1 = ",res1 );
 			document.write(" x2 = ",res2 );
-    }
+		}
+	
 }
 
  if (a == 0 && b !=0 && c != 0)
 {
 	x = -c / b;
+
 }
 
 if (a != 0 && b == 0 && c != 0)
@@ -65,4 +70,5 @@ if (a != 0 && b == 0 && c != 0)
 		}
 		document.write("x1 = ",x ); 
 		document.write(" x2 = -",x );
+
 }
