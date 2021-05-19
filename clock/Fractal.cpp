7c77b4f6548c19d703eade2d3c4a952f937e687d
Яна Белых <61792507+yanabelykh18@users.noreply.github.com>
2021-05-19 23:25:22 +0500

    Update Fractal.cpp

--- clock/Fractal.cpp
+++ clock/Fractal.cpp
@@ -10,34 +10,36 @@ SYSTEMTIME st;
 
 void drawClock(int hour, int min, int second) {
 	glColor3f(1, 1, 1);
-	glPointSize(5);
+	glPointSize(1);
 	glBegin(GL_POINTS);
 	glVertex2f(0, 0);
 	glEnd();
 
-	/*//draw number
-	glColor3f(0, 1, 0); //Sets the current color
-	glLineWidth(4);
+	
+	//draw number 3
+	glColor3f(0,0,0); //Sets the current color
+	glLineWidth(5);
 	for (float i = 0.01; i < 0.09; i += 0.03) {
 		glBegin(GL_LINES);
-		glVertex2f(arrow * (0.52 - i), 0.5);
-		glVertex2f(arrow * (0.52 - i), -0.5);
+		glVertex2f(arrow * (0.52 - i), 1.0);
+		glVertex2f(arrow * (0.52 - i), -1.0);
 		glEnd();
 	}
+
 	// number 9
-	glColor3f(0, 1, 0); //Sets the current color
-	glLineWidth(4);
+	glColor3f(0,0,0); //Sets the current color
+	glLineWidth(5);
 	for (float i = 0.01; i < 0.01; i += 0.01) {
 		glBegin(GL_LINES);
-		glVertex2f(arrow * (-0.44 - i), 0.5);
-		glVertex2f(arrow * (-0.48 - i), -0.5);
-		glVertex2f(arrow * (-0.50 - i), 0.5);
-		glVertex2f(arrow * (-0.50 - i), -0.5);
-		glVertex2f(arrow * (-0.48 - i), 0.5);
-		glVertex2f(arrow * (-0.44 - i), -0.5);
+		glVertex2f(arrow * (-0.44 - i), 1.0);
+		glVertex2f(arrow * (-0.48 - i), -1.0);
+		glVertex2f(arrow * (-0.50 - i), 1.0);
+		glVertex2f(arrow * (-0.50 - i), -1.0);
+		glVertex2f(arrow * (-0.48 - i), 1.0);
+		glVertex2f(arrow * (-0.44 - i), -1.0);
 		glEnd();
 	}
-	//number 6
+	/*//number 6
 	glColor3f(0, 1, 0); //Sets the current color
 	glLineWidth(4);
 	for (float i = 0.01; i < 0.01; i += 0.01) {
@@ -68,7 +70,7 @@ void drawClock(int hour, int min, int second) {
 
 
 	//draw minsector
-	glColor3f(1, 0.5, 0); //Sets the current color
+	glColor3f(0, 0, 0); //Sets the current color
 	glLineWidth(4);
 	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 6) {
 		glBegin(GL_LINES);
@@ -77,19 +79,64 @@ void drawClock(int hour, int min, int second) {
 	}
 	glEnd();
 
-	/*//draw secsector 
-	glColor3f(1, 1, 0); //Sets the current color
+	//draw secsector 
+	glColor3f(0, 0, 0); //Sets the current color
 	glLineWidth(3);
 	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 30) {
 		glBegin(GL_LINES);
 		glVertex2f(arrow * 0.65 * cos(phi), arrow * 0.65 * sin(phi));
 		glVertex2f(arrow * 0.7 * cos(phi), arrow * 0.7 * sin(phi));
 	}
-	glEnd();*/
+	glEnd();
 
 	//draw frame
 	glColor3f(1, 0, 0);
-	glLineWidth(0.5);
+	glLineWidth(0.6);
+	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 500000) {
+		glBegin(GL_LINES);
+		glVertex2f(arrow * 0.68 * cos(phi), arrow * 0.68 * sin(phi));
+		glVertex2f(arrow * 0.70 * cos(phi), arrow * 0.70 * sin(phi));
+	}
+	glEnd();
+
+	glColor3f(1, 0.27, 0);
+	glLineWidth(0.6);
+	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 500000) {
+		glBegin(GL_LINES);
+		glVertex2f(arrow * 0.70 * cos(phi), arrow * 0.70 * sin(phi));
+		glVertex2f(arrow * 0.72 * cos(phi), arrow * 0.72 * sin(phi));
+	}
+	glEnd();
+
+	glColor3f(1, 1, 0);
+	glLineWidth(0.6);
+	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 500000) {
+		glBegin(GL_LINES);
+		glVertex2f(arrow * 0.72 * cos(phi), arrow * 0.72 * sin(phi));
+		glVertex2f(arrow * 0.74 * cos(phi), arrow * 0.74 * sin(phi));
+	}
+	glEnd();
+
+	glColor3f(0, 1, 0);
+	glLineWidth(0.6);
+	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 500000) {
+		glBegin(GL_LINES);
+		glVertex2f(arrow * 0.74 * cos(phi), arrow * 0.74 * sin(phi));
+		glVertex2f(arrow * 0.76 * cos(phi), arrow * 0.76 * sin(phi));
+	}
+	glEnd();
+
+	glColor3f(0, 1, 1);
+	glLineWidth(0.6);
+	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 500000) {
+		glBegin(GL_LINES);
+		glVertex2f(arrow * 0.76 * cos(phi), arrow * 0.76 * sin(phi));
+		glVertex2f(arrow * 0.78 * cos(phi), arrow * 0.78 * sin(phi));
+	}
+	glEnd();
+
+	glColor3f(0, 0, 1);
+	glLineWidth(0.6);
 	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 500000) {
 		glBegin(GL_LINES);
 		glVertex2f(arrow * 0.78 * cos(phi), arrow * 0.78 * sin(phi));
@@ -97,8 +144,19 @@ void drawClock(int hour, int min, int second) {
 	}
 	glEnd();
 
+	glColor3f(0.5, 0, 0.5);
+	glLineWidth(0.6);
+	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 500000) {
+		glBegin(GL_LINES);
+		glVertex2f(arrow * 0.80 * cos(phi), arrow * 0.80 * sin(phi));
+		glVertex2f(arrow * 0.82 * cos(phi), arrow * 0.82 * sin(phi));
+	}
+	glEnd();
+
+	
+
 	//draw hoursector
-	glColor3f(1, 0, 0); //Sets the current color
+	glColor3f(0, 0, 0); //Sets the current color
 	glLineWidth(7);
 	for (float phi = 0.0; phi <= 2 * pi; phi += pi / 2) {
 		glBegin(GL_LINES);
@@ -108,7 +166,7 @@ void drawClock(int hour, int min, int second) {
 	glEnd();
 
 	//draw secArrow
-	glColor3f(0.75, 0.75, 0); //Sets the current color
+	glColor3f(0, 0, 0); //Sets the current color
 	glLineWidth(2);
 	glBegin(GL_LINE_STRIP);
 	glVertex2f(0, 0);
@@ -117,7 +175,7 @@ void drawClock(int hour, int min, int second) {
 
 
 	//draw minArrow
-	glColor3f(0.75, 0.37, 0);
+	glColor3f(0, 0, 0);
 	glLineWidth(3);
 	glBegin(GL_LINE_STRIP);
 	glVertex2f(0, 0);
@@ -125,7 +183,7 @@ void drawClock(int hour, int min, int second) {
 	glEnd();
 
 	//draw hourArrow
-	glColor3f(0.75, 0, 0);
+	glColor3f(0, 0, 0);
 	glLineWidth(4);
 	glBegin(GL_LINE_STRIP);
 	glVertex2f(0, 0);
